dd0e4f9c88badd025ef6547ee2766aa56e02fd1a
Joshua Turner <joshflyer5tp@gmail.com>
2018-08-28 00:34:35 -0400

    book @ step 4

--- src/test/java/com/jman4747/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/com/jman4747/iloveyouboss/ScoreCollectionTest.java
@@ -2,12 +2,23 @@ package com.jman4747.iloveyouboss;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class ScoreCollectionTest {
 
     @Test
-    public void test() {
+    public void answersArithmeticMeanOfTwoNumbers() {
+        // Arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // Act
+        int actualResult = collection.arithmeticMean();
+
+        // Assert
+        assertThat(actualResult, equalTo(6));
     }
 
 }
